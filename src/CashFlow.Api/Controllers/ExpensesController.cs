94a40963c961fa04ce4248ec49517bea7a9d17db
henrick.nogueira <ricknogueira1231@gmail.com>
2025-10-29 23:18:40 -0300

    Regras de negócio

--- src/CashFlow.Api/Controllers/ExpensesController.cs
+++ src/CashFlow.Api/Controllers/ExpensesController.cs
@@ -1,4 +1,5 @@
-﻿using CashFlow.Communication.Requests;
+﻿using CashFlow.Application.UseCases.Expenses.Register;
+using CashFlow.Communication.Requests;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,9 +12,11 @@ namespace CashFlow.Api.Controllers
         [HttpPost]
         public IActionResult Register([FromBody] RequestRegisterExpenseJson request)
         {
+            var useCase = new RegisterExpenseUseCase();
+
+            var response = useCase.Execute(request);
 
             return Created();
-
         }
 
     }
---
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -0,0 +1,14 @@
+﻿using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
+
+namespace CashFlow.Application.UseCases.Expenses.Register
+{
+    public class RegisterExpenseUseCase
+    {
+        public ResponseRegisteredExpenseJson Execute(RequestRegisterExpenseJson request)
+        {
+            // TODO: Validations
+            return new ResponseRegisteredExpenseJson();
+        }
+    }
+}
