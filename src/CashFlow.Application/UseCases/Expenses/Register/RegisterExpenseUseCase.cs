50349ff8233090c75891ced31af95ee8d2760a69
henrick.nogueira <ricknogueira1231@gmail.com>
2025-10-30 00:27:30 -0300

    Utilizando FluentValidation

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -15,28 +15,8 @@ namespace CashFlow.Application.UseCases.Expenses.Register
 
         private void Validate(RequestRegisterExpenseJson request)
         {
-            var titleIsEmpty = string.IsNullOrWhiteSpace(request.Title);
-            if(titleIsEmpty)
-            {
-                throw new ArgumentException("The title is required.");
-            }
-
-            if(request.Amount <= 0)
-            {
-                throw new ArgumentException("The Amount must be greater than zero.");
-            }
-
-            var result = DateTime.Compare(request.Date, DateTime.UtcNow);
-            if(result > 0)
-            {
-                throw new ArgumentException("Expenses cannot be for the future.");
-            }
-
-            var paymentTypeIsValid = Enum.IsDefined(typeof(PaymentType), request.PaymentType);
-            if (paymentTypeIsValid == false) 
-            {
-                throw new ArgumentException("Payment Type is not valid.");
-            }
+            var validator = new RegisterExpenseValidator();
+            var result = validator.Validate(request);
         }
     }
 }
