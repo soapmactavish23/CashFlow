bd6fec4ecbd98286961d535041c365759cb01f86
henrick.nogueira <ricknogueira1231@gmail.com>
2025-10-30 14:24:07 -0300

    Criando arquivo de resource

--- src/CashFlow.Api/Filters/ExceptionFilter.cs
+++ src/CashFlow.Api/Filters/ExceptionFilter.cs
@@ -1,4 +1,5 @@
 ﻿using CashFlow.Communication.Responses;
+using CashFlow.Exception;
 using CashFlow.Exception.ExceptionsBase;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
@@ -38,7 +39,7 @@ namespace CashFlow.Api.Filters
 
         private void ThrowUnkowError(ExceptionContext context)
         {
-            var errorResponse = new ResponseErrorJson("Unknown error");
+            var errorResponse = new ResponseErrorJson(ResourceErrorMessage.UNKOWN_ERROR);
 
             context.HttpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
             context.Result = new ObjectResult(errorResponse);
--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseValidator.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseValidator.cs
@@ -1,4 +1,5 @@
 ﻿using CashFlow.Communication.Requests;
+using CashFlow.Exception;
 using FluentValidation;
 
 namespace CashFlow.Application.UseCases.Expenses.Register
@@ -6,10 +7,10 @@ namespace CashFlow.Application.UseCases.Expenses.Register
     public class RegisterExpenseValidator : AbstractValidator<RequestRegisterExpenseJson>
     {
         public RegisterExpenseValidator() {
-            RuleFor(expense => expense.Title).NotEmpty().WithMessage("The title is required.");
-            RuleFor(expense => expense.Amount).GreaterThan(0).WithMessage("The amount must be greater than zero.");
-            RuleFor(expense => expense.Date).LessThanOrEqualTo(DateTime.UtcNow).WithMessage("Expenses cannot be for the future.");
-            RuleFor(expense => expense.PaymentType).IsInEnum().WithMessage("Payment Type is not valid.");
+            RuleFor(expense => expense.Title).NotEmpty().WithMessage(ResourceErrorMessage.TITLE_REQUIRED);
+            RuleFor(expense => expense.Amount).GreaterThan(0).WithMessage(ResourceErrorMessage.AMOUNT_MUST_BE_GREATER_THAN_ZERO);
+            RuleFor(expense => expense.Date).LessThanOrEqualTo(DateTime.UtcNow).WithMessage(ResourceErrorMessage.EXPENSES_CANNOT_FOR_THE_FUTURE);
+            RuleFor(expense => expense.PaymentType).IsInEnum().WithMessage(ResourceErrorMessage.PAYMENT_TYPE_INVALID);
         }
     }
 }
