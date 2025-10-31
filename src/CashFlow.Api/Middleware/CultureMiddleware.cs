cd477bcbe294c64e689f2bb8faf2851f6fb61854
henrick.nogueira <ricknogueira1231@gmail.com>
2025-10-30 21:10:06 -0300

    Validando qual o idioma recebido

---
+++ src/CashFlow.Api/Middleware/CultureMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using System.Globalization;
+
+namespace CashFlow.Api.Middleware
+{
+    public class CultureMiddleware
+    {
+
+        private readonly RequestDelegate _next;
+
+        public CultureMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+
+            var supportedLanguages = CultureInfo.GetCultures(CultureTypes.AllCultures).ToList();
+
+            var requestCulture = context.Request.Headers.AcceptLanguage.FirstOrDefault();
+
+            var culture = context.Request.Headers.AcceptLanguage.FirstOrDefault();
+
+            var cultureInfo = new CultureInfo("en");
+
+            if(string.IsNullOrWhiteSpace(requestCulture) == false && supportedLanguages.Exists(language => language.Name.Equals(requestCulture)))
+            {
+                cultureInfo = new CultureInfo(requestCulture);
+            }
+
+            CultureInfo.CurrentCulture = cultureInfo;
+            CultureInfo.CurrentUICulture = cultureInfo;
+
+            await _next(context);
+        }
+
+    }
+}
