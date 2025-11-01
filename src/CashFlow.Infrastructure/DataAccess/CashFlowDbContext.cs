9fc3631e00afd6a06605f2d26c9b3128d128b7fc
henrick.nogueira <ricknogueira1231@gmail.com>
2025-11-01 16:18:55 -0300

    Criando um Database Context

---
+++ src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -0,0 +1,22 @@
+﻿using CashFlow.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CashFlow.Infrastructure.DataAccess
+{
+    public class CashFlowDbContext : DbContext
+    {
+
+        public DbSet<Expense> Expenses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "Server=localhost;Database=cashflowdb;Uid=root;Pwd=123456";
+
+            var version = new Version(8, 0, 40);
+            var serverVersion = new MySqlServerVersion(version);
+
+            optionsBuilder.UseMySql(connectionString, serverVersion);
+        }
+
+    }
+}
