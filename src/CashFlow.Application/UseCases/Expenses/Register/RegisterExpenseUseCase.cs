db7dfa312f459c45a35836972e69c04fa2f12aa6
henrick.nogueira <ricknogueira1231@gmail.com>
2025-11-01 17:28:08 -0300

    Salvando uma despesa no banco

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,7 +1,9 @@
 ﻿using CashFlow.Communication.Enums;
 using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
+using CashFlow.Domain.Entities;
 using CashFlow.Exception.ExceptionsBase;
+using CashFlow.Infrastructure.DataAccess;
 
 namespace CashFlow.Application.UseCases.Expenses.Register
 {
@@ -10,6 +12,15 @@ namespace CashFlow.Application.UseCases.Expenses.Register
         public ResponseRegisteredExpenseJson Execute(RequestRegisterExpenseJson request)
         {
             Validate(request);
+
+            var entity = new Expense
+            {
+                Amount = request.Amount,
+                Date = request.Date,
+                Description = request.Description,
+                Title = request.Title,
+                PaymentType = (Domain.Enums.PaymentType) request.PaymentType,
+            };
 
             return new ResponseRegisteredExpenseJson();
         }
