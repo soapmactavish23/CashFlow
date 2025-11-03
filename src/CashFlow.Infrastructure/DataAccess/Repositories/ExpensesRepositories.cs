2c521836a3ba1b1f6f602bb4b425b522caf68fcd
henrick.nogueira <ricknogueira1231@gmail.com>
2025-11-03 18:31:00 -0300

    Criando o repositório para despesas

---
+++ src/CashFlow.Infrastructure/DataAccess/Repositories/ExpensesRepositories.cs
@@ -0,0 +1,15 @@
+﻿using CashFlow.Domain.Entities;
+using CashFlow.Domain.Repositories.Expenses;
+
+namespace CashFlow.Infrastructure.DataAccess.Repositories
+{
+    internal class ExpensesRepositories : IExpensesRepository
+    {
+        public void Add(Expense expense)
+        {
+            var dbContext = new CashFlowDbContext();
+            dbContext.Expenses.Add(expense);
+            dbContext.SaveChanges();
+        }
+    }
+}
