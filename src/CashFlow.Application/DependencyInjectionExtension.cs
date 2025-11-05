bb435a05d31bab01ccc162425ca0bfe3abdaf3eb
henrick.nogueira <ricknogueira1231@gmail.com>
2025-11-05 15:33:44 -0300

    Configurando o AutoMapper

--- src/CashFlow.Application/DependencyInjectionExtension.cs
+++ src/CashFlow.Application/DependencyInjectionExtension.cs
@@ -1,4 +1,5 @@
-﻿using CashFlow.Application.UseCases.Expenses.Register;
+﻿using CashFlow.Application.AutoMapper;
+using CashFlow.Application.UseCases.Expenses.Register;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CashFlow.Application
@@ -8,8 +9,20 @@ namespace CashFlow.Application
 
         public static void AddApplication(this IServiceCollection services) 
         {
+            AddAutoMapper(services);
+            AddUseCases(services);
+        }
+
+
+        private static void AddAutoMapper(IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(AutoMapping));
+        }
+        private static void AddUseCases(IServiceCollection services)
+        {
             services.AddScoped<IRegisterExpenseUseCase, RegisterExpenseUseCase>();
         }
+
 
     }
 }
