dd24b11d9f0feab85e93ab88216f305de23f0303
henrick.nogueira <ricknogueira1231@gmail.com>
2025-11-11 21:43:39 -0300

    Lendo os arquivos de fonte em bytes

--- src/CashFlow.Application/UseCases/Expenses/Report/Pdf/Fonts/ExpensesReportFontResolver.cs
+++ src/CashFlow.Application/UseCases/Expenses/Report/Pdf/Fonts/ExpensesReportFontResolver.cs
@@ -1,4 +1,5 @@
 ﻿using PdfSharp.Fonts;
+using System.Reflection;
 
 namespace CashFlow.Application.UseCases.Expenses.Report.Pdf.Fonts
 {
@@ -6,12 +7,28 @@ namespace CashFlow.Application.UseCases.Expenses.Report.Pdf.Fonts
     {
         public byte[]? GetFont(string faceName)
         {
-            throw new NotImplementedException();
+            var stream = ReadFontFile(faceName);
+            stream ??= ReadFontFile(FontHelper.DEFAULT_FONT);
+
+            var length = (int)stream!.Length;
+
+            var data = new byte[length];
+
+            stream.Read(buffer: data, offset: 0, count: length);
+
+            return data;
         }
 
         public FontResolverInfo? ResolveTypeface(string familyName, bool bold, bool italic)
         {
             return new FontResolverInfo(familyName);
+        }
+
+        private Stream? ReadFontFile(string faceName)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            return assembly.GetManifestResourceStream($"CashFlow.Application.UseCases.Expenses.Reports.Pdf.Fonts.{faceName}.ttf");
         }
     }
 }
