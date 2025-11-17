19cd111ef74c877710cfdf5ec43878935e444ef8
henrick.nogueira <ricknogueira1231@gmail.com>
2025-11-17 17:44:35 -0300

    Validator para senha

--- src/CashFlow.Application/UseCases/Users/PasswordValidator.cs
+++ src/CashFlow.Application/UseCases/Users/PasswordValidator.cs
@@ -1,15 +1,72 @@
-﻿using FluentValidation;
+﻿using CashFlow.Exception;
+using DocumentFormat.OpenXml.InkML;
+using FluentValidation;
 using FluentValidation.Validators;
+using System.Text.RegularExpressions;
 
 namespace CashFlow.Application.UseCases.Users
 {
-    public class PasswordValidator<T> : PropertyValidator<T, string>
+    public partial class PasswordValidator<T> : PropertyValidator<T, string>
     {
+        private const string ERROR_MESSAGE_KEY = "ErrorMessage";
         public override string Name => "PasswordValidator";
 
-        public override bool IsValid(ValidationContext<T> context, string value)
+        protected override string GetDefaultMessageTemplate(string errorCode)
         {
+            return $"{{{ERROR_MESSAGE_KEY}}}";
+        }
+
+        public override bool IsValid(ValidationContext<T> context, string password)
+        {
+
+            if(string.IsNullOrWhiteSpace(password))
+            {
+                return AppendArgumentPassword(context, password);
+            }
+
+            if(password.Length < 8)
+            {
+                return AppendArgumentPassword(context, password);
+            }
+
+            if(UpperCaseLetter().IsMatch(password) == false)
+            {
+                return AppendArgumentPassword(context, password);
+            }
+
+            if (LowerCaseLetter().IsMatch(password) == false)
+            {
+                return AppendArgumentPassword(context, password);
+            }
+
+            if (Numbers().IsMatch(password) == false)
+            {
+                return AppendArgumentPassword(context, password);
+            }
+
+            if (Symbols().IsMatch(password) == false)
+            {
+                return AppendArgumentPassword(context, password);
+            }
+
             return true;
         }
+
+        private bool AppendArgumentPassword(ValidationContext<T> context, string password)
+        {
+            context.MessageFormatter.AppendArgument(ERROR_MESSAGE_KEY, ResourceErrorMessage.INVALID_PASSWORD);
+            return false;
+        }
+
+        [GeneratedRegex(@"[A-Z]+")]
+        private partial Regex UpperCaseLetter();
+        
+        [GeneratedRegex(@"[a-z]+")]
+        private partial Regex LowerCaseLetter();
+
+        [GeneratedRegex(@"[0-9]+")]
+        private partial Regex Numbers();
+        [GeneratedRegex(@"[\!\?\*\.]+")]
+        private partial Regex Symbols();
     }
 }
--- src/CashFlow.Exception/ResourceErrorMessage.Designer.cs
+++ src/CashFlow.Exception/ResourceErrorMessage.Designer.cs
@@ -106,6 +106,15 @@ namespace CashFlow.Exception {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Your password must be at least 8 characters long, containing at least one uppercase letter, one lowercase letter, one number, and one special character (for example, !, ?, *, .)..
+        /// </summary>
+        public static string INVALID_PASSWORD {
+            get {
+                return ResourceManager.GetString("INVALID_PASSWORD", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Name not must be empty..
         /// </summary>
         public static string NAME_EMPTY {
