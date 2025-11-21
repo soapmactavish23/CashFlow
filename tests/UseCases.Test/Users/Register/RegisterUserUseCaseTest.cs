0f5d9583b7d45b73192c3002916250449fa59e9c
henrick.nogueira <ricknogueira1231@gmail.com>
2025-11-21 17:57:15 -0300

    Iniciando teste de UseCase para registrar usuários

---
+++ tests/UseCases.Test/Users/Register/RegisterUserUseCaseTest.cs
@@ -0,0 +1,27 @@
+﻿using CashFlow.Application.UseCases.Users.Register;
+using CommonTestUtilities.Requests;
+using FluentAssertions;
+
+namespace UseCases.Test.Users.Register
+{
+    public class RegisterUserUseCaseTest
+    {
+        [Fact]
+        public async Task Success()
+        {
+            var request = RequestRegisterUserJsonBuilder.Builder();
+            var useCase = CreateUseCase();
+
+            var result = await useCase.Execute(request);
+
+            result.Should().NotBeNull();
+            result.Name.Should().Be(request.Name);
+            result.Token.Should().NotBeNullOrWhiteSpace();
+        }
+
+        private RegisterUserUseCase CreateUseCase()
+        {
+            return new RegisterUserUseCase();
+        }
+    }
+}
