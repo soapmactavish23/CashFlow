e33e739da3523db9842db12f27120f92d8d9b2cc
henrick.nogueira <ricknogueira1231@gmail.com>
2025-11-26 16:37:18 -0300

    Utilizando banco de dados InMemory

--- src/CashFlow.Infrastructure/DependencyInjectionExtension.cs
+++ src/CashFlow.Infrastructure/DependencyInjectionExtension.cs
@@ -45,8 +45,7 @@ namespace CashFlow.Infrastructure
         private static void AddDbContext(IServiceCollection services, IConfiguration configuration) {
             var connectionString = configuration.GetConnectionString("Connection");
 
-            var version = new Version(8, 0, 40);
-            var serverVersion = new MySqlServerVersion(version);
+            var serverVersion = ServerVersion.AutoDetect(connectionString);
 
             services.AddDbContext<CashFlowDbContext>(config => config.UseMySql(connectionString, serverVersion));
         }
--- tests/WebApi.Test/CustomerWebApplicationFactory.cs
+++ tests/WebApi.Test/CustomerWebApplicationFactory.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using CashFlow.Infrastructure.DataAccess;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace WebApi.Test
 {
@@ -8,7 +11,18 @@ namespace WebApi.Test
 
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
-            builder.UseEnvironment("Test");
+            builder.UseEnvironment("Test")
+                .ConfigureServices(services =>
+                {
+                    services.AddDbContext<CashFlowDbContext>(config =>
+                    {
+
+                        var provider = services.AddEntityFrameworkInMemoryDatabase().BuildServiceProvider();
+
+                        config.UseInMemoryDatabase("InMemoryDbForTesting");
+                        config.UseInternalServiceProvider(provider);
+                    });
+                });
         }
 
     }
