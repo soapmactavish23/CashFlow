4a095f2d0cb51431a3dbe6b297d70b156314106a
henrick.nogueira <ricknogueira1231@gmail.com>
2025-11-30 18:26:16 -0300

    Entendendo e implementando usuário logado

---
+++ src/CashFlow.Domain/Services/LoggedUser/ILoggerUser.cs
@@ -0,0 +1,9 @@
+﻿using CashFlow.Domain.Entities;
+
+namespace CashFlow.Domain.Services.LoggedUser
+{
+    public interface ILoggerUser
+    {
+        Task<User> Get();
+    }
+}
---
+++ src/CashFlow.Infrastructure/Services/LoggedUser/LoggerUser.cs
@@ -0,0 +1,32 @@
+﻿using CashFlow.Domain.Entities;
+using CashFlow.Domain.Services.LoggedUser;
+using CashFlow.Infrastructure.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace CashFlow.Infrastructure.Services.LoggedUser
+{
+    public class LoggerUser : ILoggerUser
+    {
+        private readonly CashFlowDbContext _dbContext;
+
+        public LoggerUser(CashFlowDbContext dbContext) => _dbContext = dbContext;
+
+        public async Task<User> Get()
+        {
+            string token = "token";
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var jwtSecurityToken = tokenHandler.ReadJwtToken(token);
+
+            var identifier = jwtSecurityToken.Claims.First(claim => claim.Type == ClaimTypes.Sid).Value;
+
+            return await _dbContext
+                .Users
+                .AsNoTracking()
+                .FirstAsync(user => user.UserIdentifier == Guid.Parse(identifier)));
+        }
+    }
+}
