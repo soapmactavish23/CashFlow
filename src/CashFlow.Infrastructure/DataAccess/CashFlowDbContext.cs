8daa31cc3610121d64c1b76e50490735a52bca78
henrick.nogueira <ricknogueira1231@gmail.com>
2025-12-30 20:41:11 -0300

    Adicionando nova migration

--- src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
+++ src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -8,5 +8,13 @@ namespace CashFlow.Infrastructure.DataAccess
         public DbSet<Expense> Expenses { get; set; }
         public DbSet<User> Users { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Tag>().ToTable("Tags");
+
+        }
+
     }
 }
